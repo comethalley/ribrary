e4510402fd38871b32ea59ab1bfe83449b9b2c83
Jasonyecyec <jasonyecyec@gmail.com>
2022-03-16 18:51:08 +0800

    added login admin function

---
+++ functions&db/admin-login.php
@@ -0,0 +1,24 @@
+<?php
+//   $user = $_POST['username'];
+//   echo $user;
+if($_POST['username'] !== '' && $_POST['password'] !== ''){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    include_once  'database.php';
+    include_once  'functions.php';
+
+
+     //If any of the field is empty function will return true
+     if (emptyInputLogin($username,$password) === true) {
+        header("Location:../webpage/admin-login.php?error=emptyField");
+        exit();
+    }
+
+    loginAdmin($connect,$username,$password);
+
+
+}else{
+    header("Location:../webpage/admin-login.php?error=error");
+    exit();
+}
--- functions&db/functions.php
+++ functions&db/functions.php
@@ -67,10 +67,11 @@ function emailExist($connect,$email) {
     //get the result from database 
     $result = mysqli_stmt_get_result($stmt);
 
-    // if result has data return true and the values, if not return false
-    if(mysqli_fetch_assoc($result)){
-        $result = true;
+    // fetch the result then return it
+    if($result = mysqli_fetch_assoc($result)){
+
         return $result;
+        
     }else{
         $result = false;
         return $result;
@@ -127,8 +128,8 @@ function loginUser($connect,$username,$pass){
     $pwdHashed = $userExist['password'];
     $checkpwd = password_verify($pass,$pwdHashed)."\n";
 
-    //check if password match
-    if($checkpwd == true){
+    //if checkpwd has vale and equals 1
+    if($checkpwd !== '' && $checkpwd == 1){
 
         //start session and get data from userExist then store in session   
         session_start();
@@ -160,7 +161,7 @@ function upload_docu($connect, $fileName, $fileTmpName, $createdBy) {
     }
 
     //assign the variable to the placeholder and execute
-    mysqli_stmt_bind_param($stmt,$fileName, $fileTmpName, $createdBy);
+    mysqli_stmt_bind_param($stmt,'sss',$fileName, $fileTmpName, $createdBy);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
 
@@ -168,4 +169,69 @@ function upload_docu($connect, $fileName, $fileTmpName, $createdBy) {
     //if sucess uploading file, go to this 👇 page
     header("Location: ../webpage/upload-document.php?uploadsuccess");
     exit();
+}
+
+
+function adminExist($connect,$username) {
+    //template
+    $sql = "SELECT * FROM tbl_admin WHERE  username = ?;";
+
+    //prepared statement
+    $stmt = mysqli_stmt_init($connect);
+
+    //check if preparation fail 
+    if(!mysqli_stmt_prepare($stmt,$sql)){
+        header("Location:../admin-login.php?error=stmtfail");
+        exit();
+    }
+
+    //assign the variable to the placeholder and execute
+    mysqli_stmt_bind_param($stmt,"s",$username);
+    mysqli_stmt_execute($stmt);
+
+    //get the result from database 
+    $result = mysqli_stmt_get_result($stmt);
+
+    // fetch the result from database and return it
+    if($result = mysqli_fetch_assoc($result)){
+        return $result;
+    }else{
+        $result = false;
+        return $result;
+    }
+    mysqli_stmt_close($stmt);
+}
+
+function loginAdmin($connect,$username,$pass){
+    //pass the data to userExst
+    $userExist = adminExist($connect,$username);
+
+    //check if it has data if not return false 
+    if($userExist == false){
+        header("Location:../webpage/admin-login.php?error=wrongUser");
+        exit();
+    }
+
+    //hashed the password from database 
+    $passFromDB = $userExist['password'];
+
+    //conditional
+    if($passFromDB == $pass){
+
+        //start session and get data from userExist then store in session   
+        session_start();
+        $_SESSION["userFirst"] = $userExist["First_Name"];
+        $_SESSION["userLast"] = $userExist["Last_Name"];
+
+        //if sucess creating user, go to this 👇 page
+        header("Location:../webpage/LandingPage.html?LoginSucesfully!");
+        exit();
+
+    }else{
+        //if password not match from user
+        header("Location:../webpage/admin-login.php?error=passNotMatch");
+        exit();
+    }
+
+
 }
