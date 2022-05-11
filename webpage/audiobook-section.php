b306fc83f83c61222e128755dd58c3aa40486a0e
comethalley <83149734+comethalley@users.noreply.github.com>
2022-05-11 10:58:53 +0800

    content

--- webpage/audiobook-section.php
+++ webpage/audiobook-section.php
@@ -19,20 +19,13 @@ if (!isset($_SESSION['id'])) {
     <link rel="stylesheet" href="css/transitions.css">
     <link rel="stylesheet" href="css/box.css">
     <link rel="stylesheet" href="css/footer.css">
-    <!-- BOOTSTRAP CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Audiobook</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 </head>
 
 <body>
   <header>
     <div class="container">
-      <div class="list">
-        <button class="links">Settings</button>
-        <button class="links">Help & Support</button>
-        <button class="links">Log out</button>
-      </div>
-      <button class="click">...</button>
       <?php
       if (isset($_SESSION['first-name']) && isset($_SESSION['last-name']) && isset($_SESSION['email'])) {
 
@@ -45,74 +38,218 @@ if (!isset($_SESSION['id'])) {
       <?php
       }
       ?>
-    </div>
-
-    <script>
-      let click = document.querySelector('.click');
-      let list = document.querySelector('.list');
-      click.addEventListener("click", () => {
-        list.classList.toggle('newlist');
-      });
-    </script>
+    </div> 
   </header>
+    <nav class="navbar sticky-top navbar-expand-lg navbar-dark mx-0 w-100" style="background-color: #485665;">
+      <a class="navbar-brand" href="books-section.php">
+      <img src="img/ribrary-logo-white.png" width="50" height="50" class="d-inline-block" alt="logo.png">
+        Ribrary
+      </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav ml-3 mr-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="books-section.php">Ebook</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="audiobook-section.php">Audiobook<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="podcast-section.php">Podcast</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="document-section.php">Research Document</a>
+            </li>
 
-  <div class="sticky-section">
-    <div id="logo">Ribrary</div>
-        <nav> 
-          <a href="podcast-section.php">Podcast</a>
-          <a href="magazine-section.php">Magazine</a>
-          <a href="document-section.php">Document</a>
-          <a href="audiobook-section.php">Audiobook</a>
-          <a href="books-section.php">Books</a>
-        </nav>
-  </div>
+          </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search">
+                <button class="btn btn btn-outline-light my-2 my-sm-0 mr-5" type="submit">Search</button>
+            </form>    
+    </nav>
+    <div class="dropdown my-3 mx-3 float-right">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
+        Categories
+        </button>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item text-dark" href="#">Scince Fiction</a>
+          <a class="dropdown-item text-dark" href="#">Fantasy</a>
+          <a class="dropdown-item text-dark" href="#">Mystery</a>
+          <a class="dropdown-item text-dark" href="#">Horror</a>
+          <a class="dropdown-item text-dark" href="#">Adventure</a>
+          <a class="dropdown-item text-dark" href="#">Romance</a>
+        </div>
+    </div>    
   <main>
-    <div class="backdrop">
+    <!--<div class="backdrop">
     <button class="upbtn" onclick="window.location.href='upload-audiobook-section.php'">Upload your own audiobook here!</button>
-    <center>
+    <center>-->
    <!--  <button type="button" class="btn">Add Forum</button></p> -->
    </center>
   <main>
-  <div class = "container1">
-  <div class = "row text-center py-5">
+<div class="container1 mx-2 my-5">
+  <div class = "row">
+    <div class="col-md-12">
+      <h6 class =" display-4 mx-3">Newly Release</h6>
+      <hr>
+    </div>
+  </div>
+  <div class="row mb-3 ml-2">
     <?php
-            $data = $user->displayUser();
-            $count = 1;
-            foreach ($data as $row) {
-            ?>
-      <div class="col-md-3 col-sm-6 my-2 my-md-3 rounded">
-        <div class="card shadow">
+      $data = $user->displayUser();
+      $count = 1;
+      foreach ($data as $row) {
+    ?>
+      <div class="col-md-3 my-2 my-md-3 rounded">
+        <div class="card shadow text-center">
           <div>
-            <img src="img/book-icon.png" alt="Example" id = "img1">
+            <img src="img/book-icon.png" class="card-img-top" alt="book-cover">
           </div>
           <div class="card-body">
-            <h5 class = "card-title"><?php echo $row["BookName"] ?></h5>
-            <h6>
-              Ratings
-            </h6>
-            <h6 class = "author-name">
-              <?php echo $row["createdBy"] ?>
-            </h6>
-            <button class="btn">
-                  <a href = "view.html?file=<?php echo $row["BookPath"] ?>">
-                    View
-                  </a>
-            </button>
+              <p class="card-title"><?php echo $row["BookName"] ?></p>
+              <p class="card-text">Ratings</p>
+              <p class="card-text"><?php echo $row["createdBy"] ?></p>
+              <a href="view.html?file=<?php echo $row["BookPath"] ?>" class="btn btn-primary">Listen</a>
+          </div>
         </div>
       </div>
-    </div>
-            <?php
-                $count++;
+      <?php
+        $count++;
             }
-            ?>     
+      ?>
+    </div>
+    <div class = "row">
+    <div class="col-md-12">
+      <h6 class =" display-4 mx-3">Most Popular</h6>
+      <hr>
+    </div>
   </div>
-</div>
+    <div class="row mb-3 ml-2">
+      <?php
+        $data = $user->displayUser();
+        $count = 1;
+        foreach ($data as $row) {
+      ?>
+        <div class="col-md-3 col-sm-6 my-2 my-md-3 rounded">
+          <div class="card shadow text-center">
+            <div>
+              <img src="img/book-icon.png" class="card-img-top" alt="book-cover">
+            </div>
+            <div class="card-body">
+                <p class="card-title"><?php echo $row["BookName"] ?></p>
+                <p class="card-text">Ratings</p>
+                <p class="card-text"><?php echo $row["createdBy"] ?></p>
+                <a href="view.html?file=<?php echo $row["BookPath"] ?>" class="btn btn-primary">Listen</a>
+            </div>
+          </div>
+        </div>
+        <?php
+          $count++;
+              }
+        ?>
+      </div>
+      <div class = "row">
+    <div class="col-md-12">
+      <h6 class =" display-4 mx-3">Science Fiction</h6>
+      <hr>
+    </div>
+  </div>
+      <div class="row mb-3 ml-2">
+        <?php
+          $data = $user->displayUser();
+          $count = 1;
+          foreach ($data as $row) {
+        ?>
+          <div class="col-md-3 col-sm-6 my-2 my-md-3 rounded">
+            <div class="card shadow text-center">
+              <div>
+                <img src="img/book-icon.png" class="card-img-top" alt="book-cover">
+              </div>
+              <div class="card-body">
+                  <p class="card-title"><?php echo $row["BookName"] ?></p>
+                  <p class="card-text">Ratings</p>
+                  <p class="card-text"><?php echo $row["createdBy"] ?></p>
+                  <a href="view.html?file=<?php echo $row["BookPath"] ?>" class="btn btn-primary">Listen</a>
+              </div>
+            </div>
+          </div>
+          <?php
+            $count++;
+                }
+          ?>
+      </div>
+    <div class = "row">
+      <div class="col-md-12">
+        <h6 class =" display-4 mx-3">Fantasy<h6>
+        <hr>
+      </div>
+    </div>
+    <div class="row mb-3 ml-2">
+      <?php
+        $data = $user->displayUser();
+        $count = 1;
+        foreach ($data as $row) {
+      ?>
+        <div class="col-md-3 col-sm-6 my-2 my-md-3 rounded">
+          <div class="card shadow text-center">
+            <div>
+              <img src="img/book-icon.png" class="card-img-top" alt="book-cover">
+            </div>
+            <div class="card-body">
+                <p class="card-title"><?php echo $row["BookName"] ?></p>
+                <p class="card-text">Ratings</p>
+                <p class="card-text"><?php echo $row["createdBy"] ?></p>
+                <a href="view.html?file=<?php echo $row["BookPath"] ?>" class="btn btn-primary">Listen</a>
+            </div>
+          </div>
+        </div>
+        <?php
+          $count++;
+              }
+        ?>
+      </div>
+    <div class = "row">
+        <div class="col-md-12">
+          <h6 class =" display-4 mx-3">Mystery</h6>
+          <hr>
+        </div>
+    </div>
+    <div class="row mb-3 ml-2">
+      <?php
+        $data = $user->displayUser();
+        $count = 1;
+        foreach ($data as $row) {
+      ?>
+        <div class="col-md-3 col-sm-6 my-2 my-md-3 rounded">
+          <div class="card shadow text-center">
+            <div>
+              <img src="img/book-icon.png" class="card-img-top" alt="book-cover">
+            </div>
+            <div class="card-body">
+                <p class="card-title"><?php echo $row["BookName"] ?></p>
+                <p class="card-text">Ratings</p>
+                <p class="card-text"><?php echo $row["createdBy"] ?></p>
+                <a href="view.html?file=<?php echo $row["BookPath"] ?>" class="btn btn-primary">Listen</a>
+            </div>
+          </div>
+        </div>
+        <?php
+          $count++;
+              }
+        ?>
+      </div>
+  </div>
 </main>
 </main>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 </body>
 <center>
 <footer class="site-footer">
-              <h6>R - ibrary</h6>
+              <h6>Ribrary</h6>
             <ul class="footer-links">
               <li><a href="">About Us</a></li>
               <li><a href="">Contacts</a></li>
--- webpage/books-section.php
+++ webpage/books-section.php
@@ -15,9 +15,10 @@ include '../includes/autoload-class.php';
   <!-- <link rel="stylesheet" href="css/box.css"> -->
   <link rel="stylesheet" href="css/book-section.css">
   <link rel="stylesheet" href="css/footer.css">
-  <title>My Profile</title>
+  <title>Ebooks</title>
 
   <title>Books</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 </head>
 
@@ -92,21 +93,54 @@ include '../includes/autoload-class.php';
     </script>
   </header>
 
-  <div class="sticky-section">
-    <div id="logo">Ribrary</div>
-    <nav>
-      <a href="podcast-section.php">Podcast</a>
-      <a href="magazine-section.php">Magazine</a>
-      <a href="document-section.php">Document</a>
-      <a href="audiobook-section.php">Audiobook</a>
-      <a href="books-section.php">Ebooks</a>
-    </nav>
-  </div>
+  <nav class="navbar sticky-top navbar-expand-lg navbar-dark mx-0 w-100" style="background-color: #485665;">
+      <a class="navbar-brand" href="books-section.php">
+      <img src="img/ribrary-logo-white.png" width="50" height="50" class="d-inline-block" alt="logo.png">
+        Ribrary
+      </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav ml-3 mr-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" href="books-section.php">Ebook<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="audiobook-section.php">Audiobook</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="podcast-section.php">Podcast</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="document-section.php">Research Document</a>
+            </li>
 
+          </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search">
+                <button class="btn btn btn-outline-light my-2 my-sm-0 mr-5" type="submit">Search</button>
+            </form>    
+    </nav>
+    <div class="dropdown my-3 mx-3 float-right">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
+        Categories
+        </button>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item text-dark" href="#">Scince Fiction</a>
+          <a class="dropdown-item text-dark" href="#">Fantasy</a>
+          <a class="dropdown-item text-dark" href="#">Mystery</a>
+          <a class="dropdown-item text-dark" href="#">Horror</a>
+          <a class="dropdown-item text-dark" href="#">Adventure</a>
+          <a class="dropdown-item text-dark" href="#">Romance</a>
+        </div>
+    </div> 
+    
   <main>
     <div class="backdrop">
-      <button class="upbtn" onclick="window.location.href='upload-books-section.php'">Upload your
-        own book here!</button>
+      <!--<button class="upbtn" onclick="window.location.href='upload-books-section.php'">Upload your
+        own book here!</button>-->
       <center>
         <!-- <button type="button" class="btn">Add Book</button></p> -->
       </center>
@@ -145,6 +179,10 @@ include '../includes/autoload-class.php';
     })
   </script>
   <script src="js/books-section.js"></script>
+
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
 </body>
 
