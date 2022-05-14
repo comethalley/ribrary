9d6c1173902989f19f0dc64787ed2c955223dc8a
Jasonyecyec <jasonyecyec@gmail.com>
2022-05-14 22:39:54 +0800

    accept decline notif

--- functions/admin-pendingDocs-function.php
+++ functions/admin-pendingDocs-function.php
@@ -7,6 +7,10 @@ if (isset($_POST['accept-docs'])) {
     $doc_id = $_POST['doc_id'];
 
     if (!$doc_id) return;
+    $data = $admin->getResearchDocument($doc_id);
+    if ($data) {
+        $admin->notification($data['doc_name'], $data['doc_file'], $data['doc_path'], $data['createdBy'], $data['user_id'], 'accepted', 'unread');
+    }
 
     $admin->accept_documents($doc_id);
 }
@@ -17,6 +21,10 @@ if (isset($_POST['decline-docs'])) {
     $message = $_POST['decline-text'];
 
     if (!$doc_id) return;
+    $data = $admin->getResearchDocument($doc_id);
+    if ($data) {
+        $admin->notification($data['doc_name'], $data['doc_file'], $data['doc_path'], $data['createdBy'], $data['user_id'], 'declined', 'unread', $message);
+    }
 
     $admin->decline_documents($doc_id, $message);
 }
